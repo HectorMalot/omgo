9849162287f355109c58b8d54fc4747039e9a1ca
Dennis de Reus <dennisdereus@gmail.com>
2021-09-21 12:09:52 +0200

    discard unused parameters for CurrentWeather so that we don't request metrics that will go unused

--- current_weather.go
+++ current_weather.go
@@ -9,6 +9,11 @@ import (
 // Units and timezones can be provided using an optional `Options` parameter.
 // Any requested hourly or daily metrics as part of the options are discarded
 func (c Client) CurrentWeather(ctx context.Context, loc Location, opts *Options) (CurrentWeather, error) {
+	// Discard requested daily/hourly metrics, as they are not returned as part of the current weather
+	if opts != nil {
+		opts.DailyMetrics = nil
+		opts.HourlyMetrics = nil
+	}
 
 	body, err := c.Get(ctx, loc, opts)
 	if err != nil {
