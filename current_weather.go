3cc2d304d52d15f6fb1efd1086b4bebb28d46a58
Dennis de Reus <dennisdereus@gmail.com>
2021-09-21 12:08:10 +0200

    improve documentation of Forecast and CurrentWeather

--- current_weather.go
+++ current_weather.go
@@ -4,6 +4,10 @@ import (
 	"context"
 )
 
+// CurrentWeather returns the current weather for the provided location
+//
+// Units and timezones can be provided using an optional `Options` parameter.
+// Any requested hourly or daily metrics as part of the options are discarded
 func (c Client) CurrentWeather(ctx context.Context, loc Location, opts *Options) (CurrentWeather, error) {
 
 	body, err := c.Get(ctx, loc, opts)
--- forecast.go
+++ forecast.go
@@ -4,6 +4,11 @@ import (
 	"context"
 )
 
+// Forecast retreives the 7 day weather forecast for the provided location.
+//
+// Use `Options` to specify which metrics to retrieve. The response is a Forecast
+// struct that will contains the current weather, all requested hourly predictions and
+// all requested daily predictions
 func (c Client) Forecast(ctx context.Context, loc Location, opts *Options) (*Forecast, error) {
 	body, err := c.Get(ctx, loc, opts)
 	if err != nil {
